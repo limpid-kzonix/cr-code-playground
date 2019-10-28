bb624871f5c28e657eac23d9247fa28b326c2601
Alexander Balyshyn <balyszyn@gmail.com>
2019-10-28 16:31:47 +0200

    Initial commit

---
+++ spec/cr-code-playground_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Cr::Code::Playground do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/cr-code-playground.cr
@@ -0,0 +1,13 @@
+# TODO: Write documentation for `Cr::Code::Playground`
+require "http/server"
+
+
+port = 9190
+
+server = HTTP::Server.new do |context|
+    context.response.content_type = "text/html"
+    context.response.print "Hello world, got #{context.request.path}"
+end
+
+puts "Listening on port :#{port}"
+server.listen(port)
